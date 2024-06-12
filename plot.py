8052d22f241a5d1773cec9c97685a8ca87762e69
Areebol <103118945+Areebol@users.noreply.github.com>
2024-06-12 22:30:12 +0800

    Updated png

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data = pd.read_csv("output.csv")
+data = pd.read_csv("llama2_7b_poem.csv")
 
 plt.figure(figsize=(10, 6))
 plt.scatter(data['Token_Entropy'], data['Acc'], color='blue')
@@ -8,4 +8,20 @@ plt.title('Scatter Plot of Token Entropy vs Accuracy')
 plt.xlabel('Token Entropy')
 plt.ylabel('Accuracy')
 plt.grid(True)
-plt.savefig("tmp.png")
+plt.savefig("llama2_7b_poem_1.png")
+
+plt.figure(figsize=(10, 6))
+plt.scatter(data['Sentence_Entropy'], data['Acc'], color='blue')
+plt.title('Scatter Plot of Sentence Entropy vs Accuracy')
+plt.xlabel('Sentence Entropy')
+plt.ylabel('Accuracy')
+plt.grid(True)
+plt.savefig("llama2_7b_poem_2.png")
+
+plt.figure(figsize=(10, 6))
+plt.scatter(data['Token_Entropy']/data['Sentence_Entropy'], data['Acc'], color='blue')
+plt.title('Scatter Plot of Token/Sentence Entropy vs Accuracy')
+plt.xlabel('Token/Sentence Entropy')
+plt.ylabel('Accuracy')
+plt.grid(True)
+plt.savefig("llama2_7b_poem_3.png")
